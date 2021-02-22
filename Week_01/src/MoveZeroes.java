767703b4b100b7f13305eb7cf83a39672c70a0d5
yanaijuan <yanaijuan1988@yeah.net>
2021-02-23 07:15:41 +0800

    盛最多水的容器

--- Week_01/src/MoveZeroes.java
+++ Week_01/src/MoveZeroes.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class MoveZeroes {
+/*
+题目：给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序
+思路：
+毫无思路，看来要多看两次视频，好好理解
+ */
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        if( nums == null){
+            return;
+        }
+        int j = 0;
+        for( int i = 0; i < nums.length; i++){
+            if( nums[i] != 0){
+                nums[j++] = nums[i];
+            }
+        }
+        for(int i = j; i<nums.length; i++){
+            nums[i] = 0;
+        }
+        }
+
+    public static void main(String[] args) {
+
+    }
 }
